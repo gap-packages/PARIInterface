9f8d955b488aaf6931eee67be128a9abfe83cbc0
Max Horn <max@quendi.de>
2019-02-07 11:55:29 +0100

    Fix README.md, ensure makedoc.g runs

--- gap/PARIInterface.gd
+++ gap/PARIInterface.gd
@@ -10,6 +10,7 @@ _PARIINTERFACE_STACKMAX_DEFAULT := 1024 * 1024 *1024;
 
 
 #! @Chapter PARIInterface
+#! @Section Basics
 #!
 #! A C interface to the PARI number theory library
 DeclareCategory( "IsPARIObj",  IsObject );
